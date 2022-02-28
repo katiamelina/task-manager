a7c366bbf062cd1b9ded4be0d0a9dec7312b5343
Terry Tosh <tmt103@txstate.edu>
2022-02-27 18:17:30 -0600

    Added Source Files (Main GUI)

---
+++ Task Manager Application/taskmanager/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taskmanager;
+
+/**
+ *
+ * @author 16825
+ */
+public class Main {
+    public static void main(String[] args) {
+        new TaskManagerMainGUI().setVisible(true);
+    }
+}
